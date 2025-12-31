711a2b0a853caeba00b33ebd60fc24307b32eb96
Ronnie Llamado <35092029+ronnie-llamado@users.noreply.github.com>
2025-12-31 06:34:42 -0800

    feat: add initial leds (#1)
    - port led example code
    - setup enums for hold types, colors,
    
    Co-authored-by: Ronnie Llamado <ronnie.llamado@gmail.com>

---
+++ src/moonboard.h
@@ -0,0 +1,13 @@
+#ifndef MOONBOARD_H
+#define MOONBOARD_H
+
+enum moonboard_status_e
+{
+    MOONBOARD_STATUS_SUCCESS = 0,
+    MOONBOARD_STATUS_LED_FAILURE,
+};
+
+enum moonboard_status_e moonboard_init(void);
+void moonboard_process(void);
+
+#endif // MOONBOARD_H
